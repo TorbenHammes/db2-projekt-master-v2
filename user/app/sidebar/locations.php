59c110740c0e982d7f388f2c33e8e4a9434d204f
Torben Hammes <torben@hammes.media>
2020-02-19 00:49:18 +0100

    Update

--- user/app/sidebar/locations.php
+++ user/app/sidebar/locations.php
@@ -19,6 +19,20 @@
     <!-- Slim CSS -->
     <link rel="stylesheet" href="../css/slim.css">
 
+    <!-- Leaflet JS -->
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css"
+          integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
+          crossorigin=""/>
+
+    <script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"
+            integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew=="
+            crossorigin=""></script>
+
+    <!-- Mapbox CDN -->
+    <script src='https://api.mapbox.com/mapbox-gl-js/v1.7.0/mapbox-gl.js'></script>
+    <link href='https://api.mapbox.com/mapbox-gl-js/v1.7.0/mapbox-gl.css' rel='stylesheet'/>
+
+
 </head>
 <body>
 
@@ -73,71 +87,59 @@
     </div><!-- slim-sidebar -->
 
     <div class="slim-mainpanel">
-        <div class="container">
-            <div class="slim-pageheader">
-                <ol class="breadcrumb slim-breadcrumb">
-                    <li class="breadcrumb-item"><a href="index.php">Home</a></li>
-                </ol>
-                <h6 class="slim-pagetitle">Standorte</h6>
-            </div><!-- slim-pageheader -->
+    <div class="container">
+        <div class="slim-pageheader">
+            <ol class="breadcrumb slim-breadcrumb">
+                <li class="breadcrumb-item"><a href="index.php">Home</a></li>
+            </ol>
+            <h6 class="slim-pagetitle">Standorte</h6>
+        </div><!-- slim-pageheader -->
 
-        </div><!-- container -->
+    </div><!-- container -->
 
-        <div class="section-wrapper mg-t-20">
-            <label class="section-title">Karte</label>
-            <p class="mg-b-20 mg-sm-b-40">Auf der Karte siehst du alle Standorte.</p>
-            <div class="row">
-                <div class="col">
-                    <div id="leaflet-<?php print_r($lid); ?>" style="height=200px;"
-                         class="ht-250 ht-sm-300 ht-md-400 bd bg-gray-100"></div>
-                    <script>
-                        var x = '<?php print_r($x); ?>';
-                        var y = '<?php print_r($y); ?>';
-                        var lid = '<?php print_r($lid); ?>'
+    <div class="section-wrapper mg-t-20">
+        <label class="section-title">Karte</label>
+        <p class="mg-b-20 mg-sm-b-40">Auf der Karte siehst du alle Standorte.</p>
+        <div class="row">
+            <div class="col">
+                <div id="leaflet-<?php print_r($lid); ?>" style="height=200px;"
+                     class="ht-250 ht-sm-300 ht-md-400 bd bg-gray-100"></div>
+                <script>
+                    var x = '<?php print_r($x); ?>';
+                    var y = '<?php print_r($y); ?>';
+                    var lid = '<?php print_r($lid); ?>'
 
-                        var mymap = L.map('leaflet-', lid).setView([52.408, 12.779], 10);
-                        L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-                            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-                            maxZoom: 18,
-                            id: 'mapbox/streets-v11',
-                            tileSize: 512,
-                            zoomOffset: -1,
-                            accessToken: 'pk.eyJ1IjoiaGFtbWVzbWVkaWEiLCJhIjoiY2s2c2dwYWc4MGZlZjNubnZreTB5d3F3ZiJ9.ipG2uTSEaTRPNovKrJYRmQ'
-                        }).addTo(mymap);
+                    var mymap = L.map('leaflet-', lid).setView([52.408, 12.779], 10);
+                    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+                        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+                        maxZoom: 18,
+                        id: 'mapbox/streets-v11',
+                        tileSize: 512,
+                        zoomOffset: -1,
+                        accessToken: 'pk.eyJ1IjoiaGFtbWVzbWVkaWEiLCJhIjoiY2s2c2dwYWc4MGZlZjNubnZreTB5d3F3ZiJ9.ipG2uTSEaTRPNovKrJYRmQ'
+                    }).addTo(mymap);
 
-                        var marker = L.marker([x, y]).addTo(mymap);
+                    var marker = L.marker([x, y]).addTo(mymap);
 
-                    </script>
-                </div>
+                </script>
             </div>
-        </div><!-- section-wrapper -->
-    </div>
-    <div class="slim-mainpanel">
-        <div class="container">
-            <div class="slim-pageheader">
-                <ol class="breadcrumb slim-breadcrumb">
-                    <li class="breadcrumb-item"><a href="index.php">Home</a></li>
-                </ol>
-                <h6 class="slim-pagetitle">Standorte</h6>
-            </div><!-- slim-pageheader -->
+        </div>
+    </div><!-- section-wrapper -->
+    <div class="section-wrapper mg-t-20">
+        <label class="section-title">Standortliste</label>
+        <p class="mg-b-20 mg-sm-b-40">Hier sind alle Standorte in der Nähe.</p>
 
-        </div><!-- container -->
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="list-group list-group-user">
+                    <?php
 
-        <div class="section-wrapper mg-t-20">
-            <label class="section-title">Standortliste</label>
-            <p class="mg-b-20 mg-sm-b-40">Hier sind alle Standorte in der Nähe.</p>
+                    $dblink = new mysqli('localhost', 'master-projekt-db2', 'Xj41t^h3', 'master-projekt-db2');
 
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="list-group list-group-user">
-                        <?php
-
-                        $dblink = new mysqli('localhost', 'master-projekt-db2', 'Xj41t^h3', 'master-projekt-db2');
-
-                        $sql = "select * from TBL_Locations";
-                        if ($result = $dblink->query($sql)) {
-                            while ($row = $result->fetch_row()) {
-                                print "<div class=\"list-group-item\">
+                    $sql = "select * from TBL_Locations";
+                    if ($result = $dblink->query($sql)) {
+                        while ($row = $result->fetch_row()) {
+                            print "<div class=\"list-group-item\">
                                 <img src=\"http://via.placeholder.com/500x500\" alt=\"\">
                                 <div class=\"user-name-address\">
                                 <p>Standort von $row[1] </p>
@@ -149,25 +151,25 @@
                                 </a>
                                 </div>
                                 </div>";
-                            }
-
-                        } else {
-                            print "SQL scheint falsch zu sein";
                         }
 
-                        ?>
-                    </div><!-- list-group -->
-                </div><!-- col-6 -->
-            </div><!-- section-wrapper -->
-        </div>
+                    } else {
+                        print "SQL scheint falsch zu sein";
+                    }
 
-        <div class="slim-footer mg-t-0">
-            <div class="container-fluid">
-                <p>Copyright 2020 &copy; Regio Projekt DB2</p>
-                <p>GitHub <a href="https://github.com/TorbenHammes/">Torben Hammes</a></p>
-            </div><!-- container-fluid -->
-        </div><!-- slim-footer -->
-    </div><!-- slim-mainpanel -->
+                    ?>
+                </div><!-- list-group -->
+            </div><!-- col-6 -->
+        </div><!--row-->
+    </div><!-- section-wrapper -->
+
+    <div class="slim-footer mg-t-0">
+        <div class="container-fluid">
+            <p>Copyright 2020 &copy; Regio Projekt DB2</p>
+            <p>GitHub <a href="https://github.com/TorbenHammes/">Torben Hammes</a></p>
+        </div><!-- container-fluid -->
+    </div><!-- slim-footer -->
+</div><!-- slim-mainpanel -->
 </div><!-- slim-body -->
 
 <script src="../lib/jquery/js/jquery.js"></script>
