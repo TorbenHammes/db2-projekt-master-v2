e0d1fe71e454e5ca4b72a6401daf9527d878d64f
Torben Hammes <torben@hammes.media>
2020-02-18 22:03:56 +0100

    Debugging

--- user/app/sidebar/e/p.php
+++ user/app/sidebar/e/p.php
@@ -14,6 +14,8 @@ if ($result = $dblink->query($sql)) {
 
 setlocale(LC_MONETARY, 'de_DE');
 
+$preisf = number_format($preis, 2, ',', ' ');
+
 ?>
 <!DOCTYPE html>
 <html lang="de">
@@ -110,7 +112,7 @@ setlocale(LC_MONETARY, 'de_DE');
                         <p class="mg-b-20 mg-sm-b-40"><?php print_r($beschr); ?></p>
                         <div class="card">
                             <div class="card-body">
-                                <h5 class="card-title tx-dark tx-medium mg-b-10"><? print($preis); ?></h5>
+                                <h5 class="card-title tx-dark tx-medium mg-b-10"><? printf($preisf); ?></h5>
                                 <p class="card-subtitle tx-normal mg-b-15">Preis in &euro;</p>
                             </div>
                         </div><!-- card -->
